24dcaab57fbb9c29302e204ca3434492f9cedd5c
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-01-12 20:55:23 -0300

    Tests Junit

---
+++ testsJunit/src/test/java/io/github/marrafon91/testsJunit/TestsJunitApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.marrafon91.testsJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
