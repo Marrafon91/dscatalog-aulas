e0fa6afc131f2c899fbb149e7a2090ee8c49ecf4
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-01-17 08:24:51 -0300

    Primeiro teste, simulando comportamento com Mockito

---
+++ backend-testes-automatizado/src/test/java/io/github/marrafon91/dscatalog/service/ProductServiceTests.java
@@ -0,0 +1,45 @@
+package io.github.marrafon91.dscatalog.service;
+
+import io.github.marrafon91.dscatalog.repositories.ProductRepository;
+import io.github.marrafon91.dscatalog.services.ProductService;
+import io.github.marrafon91.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService service;
+
+    @Mock
+    private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp() {
+        existingId = 1L;
+        nonExistingId = 1000L;
+
+        Mockito.doNothing().when(repository).deleteById(existingId);
+//        Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+    }
+
+    @Test
+    void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+        assertThrows(ResourceNotFoundException.class,
+                () -> service.delete(nonExistingId)
+        );
+    }
+}
