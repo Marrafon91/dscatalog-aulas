0d24f3255e97691aa3fcf7d6acaff2c84b15f94d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-01-19 07:11:47 -0300

    Teste delete lança DatabaseException quando id dependente

--- backend-testes-automatizado/src/test/java/io/github/marrafon91/dscatalog/service/ProductServiceTests.java
+++ backend-testes-automatizado/src/test/java/io/github/marrafon91/dscatalog/service/ProductServiceTests.java
@@ -2,12 +2,14 @@ package io.github.marrafon91.dscatalog.service;
 
 import io.github.marrafon91.dscatalog.repositories.ProductRepository;
 import io.github.marrafon91.dscatalog.services.ProductService;
+import io.github.marrafon91.dscatalog.services.exceptions.DatabaseException;
 import io.github.marrafon91.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -25,22 +27,33 @@ public class ProductServiceTests {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 3L;
 
         doNothing().when(repository).deleteById(existingId);
+        doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+
         when(repository.existsById(existingId)).thenReturn(true);
         when(repository.existsById(nonExistingId)).thenReturn(false);
+        when(repository.existsById(dependentId)).thenReturn(true);
+    }
+
+    @Test
+    void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        assertThrows(DatabaseException.class, () -> {
+            service.delete(dependentId);
+        });
     }
 
     @Test
     void deleteShouldDoNothingWhenIdExist() {
         assertDoesNotThrow(() -> service.delete(existingId)
         );
-
         verify(repository, times(1)).deleteById(existingId);
     }
 
