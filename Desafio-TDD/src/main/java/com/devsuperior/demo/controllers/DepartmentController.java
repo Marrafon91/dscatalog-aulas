2ca1bfa3a7f833907b5ccd56b8a9e58f93a5882d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-01-23 06:53:31 -0300

    Department findAll implements

--- Desafio-TDD/src/main/java/com/devsuperior/demo/controllers/DepartmentController.java
+++ Desafio-TDD/src/main/java/com/devsuperior/demo/controllers/DepartmentController.java
@@ -1,10 +1,26 @@
 package com.devsuperior.demo.controllers;
 
+import com.devsuperior.demo.dto.DepartmentDTO;
+import com.devsuperior.demo.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
-@RequestMapping("departments")
+@RequestMapping("/departments")
 public class DepartmentController {
+
+    @Autowired
+    DepartmentService service;
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentDTO>> findAll() {
+        List<DepartmentDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 }
--- Desafio-TDD/src/main/java/com/devsuperior/demo/service/DepartmentService.java
+++ Desafio-TDD/src/main/java/com/devsuperior/demo/service/DepartmentService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.demo.service;
 
+import com.devsuperior.demo.dto.DepartmentDTO;
 import com.devsuperior.demo.entities.Department;
 import com.devsuperior.demo.repositories.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static org.springframework.data.domain.Sort.by;
+
 @Service
 public class DepartmentService {
 
     @Autowired
     DepartmentRepository repository;
 
-    public List<Department> list =
+    public List<DepartmentDTO> findAll() {
+        List<Department> list = repository.findAll(by("name"));
+        return list.stream().map(DepartmentDTO::new).toList();
+    }
 }
