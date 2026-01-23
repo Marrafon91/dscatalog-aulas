ec28730e8459b6e3b1d46ff4e1cbff3eb2158349
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-01-23 12:02:31 -0300

    eventCity project cloned

---
+++ eventCity/src/main/java/com/devsuperior/bds02/TDDEventCity.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bds02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDDEventCity {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDDEventCity.class, args);
+	}
+
+}
