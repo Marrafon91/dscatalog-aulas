9bc7386490a55e29684e19da2505fd7c01338ff5
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-01-25 17:30:29 -0300

    Link JPA Query Methods

--- eventCity/src/main/java/com/devsuperior/bds02/repositories/CityRepository.java
+++ eventCity/src/main/java/com/devsuperior/bds02/repositories/CityRepository.java
@@ -3,5 +3,9 @@ package com.devsuperior.bds02.repositories;
 import com.devsuperior.bds02.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Link JPA Query Methods
+//https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+
 public interface CityRepository extends JpaRepository<City, Long> {
+
 }
