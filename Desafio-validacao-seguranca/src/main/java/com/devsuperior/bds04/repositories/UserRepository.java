ead4359a029f8d770c4922222244726b042a732e
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-02-06 09:53:05 -0300

    City service and City controller

--- Desafio-validacao-seguranca/src/main/java/com/devsuperior/bds04/repositories/UserRepository.java
+++ Desafio-validacao-seguranca/src/main/java/com/devsuperior/bds04/repositories/UserRepository.java
@@ -1,7 +1,13 @@
 package com.devsuperior.bds04.repositories;
 
 import com.devsuperior.bds04.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "roles")
+    Optional<User> findByEmail(String email);
 }
---
+++ Desafio-validacao-seguranca/src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.by;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll(by("name"));
+        return list.stream().map(CityDTO::new).toList();
+    }
+
+    @Transactional
+    public CityDTO insert(@RequestBody CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        repository.save(entity);
+        return new CityDTO(entity);
+    }
+}
